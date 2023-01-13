d8a3202bde77d45441c5edb2c33de3ab843db766
mezitest <mezis4u@inbox.ru>
2023-01-14 01:04:39 +0500

    Add tests for findLast

--- src/test/java/ru/netology/manager/MovieTestNotDefault.java
+++ src/test/java/ru/netology/manager/MovieTestNotDefault.java
@@ -1,6 +1,5 @@
 package ru.netology.manager;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Assertions;
 import ru.netology.data.Poster;
 
@@ -9,16 +8,13 @@ public class MovieTestNotDefault {
     Poster poster1 = new Poster("Бладшот", "3024", "Боевик", "????");
     Poster poster2 = new Poster("Вперёд", "7302", "Мультфильм", "????");
     Poster poster3 = new Poster("Отель Белград", "5629", "Комедия", "????");
-
-    @BeforeEach
-    public void setup() {
-        manager.addMovie(poster1);
-        manager.addMovie(poster2);
-        manager.addMovie(poster3);
-    }
+    Poster poster4 = new Poster("Джентельмены", "2480", "Боевик", "????");
 
     @Test
     public void shouldAddNewMovies() {
+        manager.addMovie(poster1);
+        manager.addMovie(poster2);
+        manager.addMovie(poster3);
 
         Poster[] expected = new Poster[] { poster1, poster2, poster3 };
         Poster[] actual = manager.getPosters();
@@ -28,6 +24,9 @@ public class MovieTestNotDefault {
 
     @Test
     public void shouldShowAddedMoviesInAddingOrder() {
+        manager.addMovie(poster1);
+        manager.addMovie(poster2);
+        manager.addMovie(poster3);
 
         Poster[] expected = new Poster[] { poster1, poster2, poster3 };
         Poster[] actual = manager.findAll();
@@ -36,9 +35,55 @@ public class MovieTestNotDefault {
     }
 
     @Test
-    public void shouldFindLastAddedMovies() {
+    public void shouldFindLastAddedMoviesLimit() {
+        manager.addMovie(poster1);
+        manager.addMovie(poster2);
+        manager.addMovie(poster3);
 
-        Poster[] expected = new Poster[]{ poster3, poster2, poster1 };
+        Poster[] expected = new Poster[] { poster3, poster2, poster1 };
+        Poster[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindLastAddedMoviesAboveLimit() {
+        manager.addMovie(poster1);
+        manager.addMovie(poster2);
+        manager.addMovie(poster3);
+        manager.addMovie(poster4);
+
+        Poster[] expected = new Poster[] { poster4, poster3, poster2 };
+        Poster[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindLastAddedMoviesUnderLimit() {
+        manager.addMovie(poster1);
+        manager.addMovie(poster2);
+
+        Poster[] expected = new Poster[] { poster2, poster1 };
+        Poster[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotFindLastWhen0moviesAdded() {
+
+        Poster[] expected = new Poster[]{ };
+        Poster[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindLastWhen1MoviesAdded() {
+        manager.addMovie(poster1);
+
+        Poster[] expected = new Poster[] { poster1 };
         Poster[] actual = manager.findLast();
 
         Assertions.assertArrayEquals(expected, actual);
--- src/test/java/ru/netology/manager/MoviesTest.java
+++ src/test/java/ru/netology/manager/MoviesTest.java
@@ -15,6 +15,7 @@ public class MoviesTest {
     Poster poster8 = new Poster("Рыцари справедливости", "1963", "Драма", "????");
     Poster poster9 = new Poster("Момент истины", "3836", "Триллер", "????");
     Poster poster10 = new Poster("Довод", "5491", "Боевик", "????");
+    Poster poster11 = new Poster("Не смотрите наверх", "2801", "Драма", "????");
 
     MoviesManager manager = new MoviesManager();
 
@@ -51,7 +52,7 @@ public class MoviesTest {
     }
 
     @Test
-    public void shouldFindLastAddedMovies() {
+    public void shouldFindLastAddedMoviesLimit() {
 
         manager.addMovie(poster1);
         manager.addMovie(poster2);
@@ -64,7 +65,87 @@ public class MoviesTest {
         manager.addMovie(poster9);
         manager.addMovie(poster10);
 
-        Poster[] expected = new Poster[] { poster10, poster9, poster8, poster7, poster6, poster5, poster4, poster3, poster2, poster1 };
+        Poster[] expected = new Poster[] {
+                poster10,
+                poster9,
+                poster8,
+                poster7,
+                poster6,
+                poster5,
+                poster4,
+                poster3,
+                poster2,
+                poster1
+        };
+
+        Poster[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindLastAddedMoviesUnderLimit() {
+
+        manager.addMovie(poster1);
+        manager.addMovie(poster2);
+        manager.addMovie(poster3);
+        manager.addMovie(poster4);
+        manager.addMovie(poster5);
+        manager.addMovie(poster6);
+
+
+        Poster[] expected = new Poster[] { poster6, poster5, poster4, poster3, poster2, poster1 };
+        Poster[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindLastAddedMoviesAboveLimit() {
+
+        manager.addMovie(poster1);
+        manager.addMovie(poster2);
+        manager.addMovie(poster3);
+        manager.addMovie(poster4);
+        manager.addMovie(poster5);
+        manager.addMovie(poster6);
+        manager.addMovie(poster7);
+        manager.addMovie(poster8);
+        manager.addMovie(poster9);
+        manager.addMovie(poster10);
+        manager.addMovie(poster11);
+
+        Poster[] expected = new Poster[] {
+                poster11,
+                poster10,
+                poster9,
+                poster8,
+                poster7,
+                poster6,
+                poster5,
+                poster4,
+                poster3,
+                poster2
+        };
+        Poster[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotFindLastWhen0MoviesAdded() {
+
+        Poster[] expected = new Poster[] { };
+        Poster[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindLastWhen1MoviesAdded() {
+        manager.addMovie(poster1);
+
+        Poster[] expected = new Poster[] { poster1 };
         Poster[] actual = manager.findLast();
 
         Assertions.assertArrayEquals(expected, actual);
